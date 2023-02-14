de4087d6f5bcfabab9f0a83293a6a23d62d5bd09
Václav Dona <vasekdona@seznam.cz>
2023-02-14 21:23:14 +0100

    v 0.3

--- Minesweeper/game/main.py
+++ Minesweeper/game/main.py
@@ -5,27 +5,139 @@ from pygame.locals import (
     K_ESCAPE,
     KEYDOWN,
     QUIT,
+    MOUSEBUTTONDOWN,
 )
 
 SCREEN_WIDTH = 770
 SCREEN_HEIGHT = 881
+
+pygame.init()
+
+font = pygame.font.SysFont("Arial", 20)
+
+# Set up the drawing window
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pygame.display.set_caption('Minesweeper')
+
+victory = 0
+defeat = 0
 
 class Field(pygame.sprite.Sprite):
     def __init__(self):
         super(Field, self).__init__()
         self.surf = pygame.image.load("field.jpg").convert()
         self.surf_click = pygame.image.load("field_click.jpg").convert()
-        self.surf_mine = pygame.image.load("mine.jpg").convert()
+        self.surf_mine = pygame.image.load("mine.JPG").convert()
+        self.surf_mine_explode = pygame.image.load("mine_explode.jpg").convert()
+        self.flag = pygame.image.load("flag.jpg").convert()
+        self.flag_wrong = pygame.image.load("flag_wrong.jpg").convert()
+        self.grid = pygame.image.load("grid.jpg").convert()
+        self.one = pygame.image.load("one.jpg").convert()
+        self.two = pygame.image.load("two.jpg").convert()
+        self.three = pygame.image.load("three.jpg").convert()
+        self.four = pygame.image.load("four.jpg").convert()
+        self.five = pygame.image.load("five.jpg").convert()
+        self.six = pygame.image.load("six.jpg").convert()
+        self.seven = pygame.image.load("seven.jpg").convert()
+        self.eight = pygame.image.load("eight.jpg").convert()
         self.position_x = counter_x
         self.position_y = counter_y
+        self.button_rect = pygame.Rect(self.position_x, self.position_y, 44, 44)
         self.mine = is_mine
+        self.clicked = 0
+        self.number_of_mines = 0
+        self.index_num = 0
+
+    def index_number(self):
+        self.index_num = str(field_list.index(self))
+
+    def scan(self):
+        number = 0
+        try:
+            if field_list[field_list.index(self) - 17].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) - 16].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) - 15].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) - 1].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) + 1].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) + 15].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) + 16].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+        try:
+            if field_list[field_list.index(self) + 17].mine == 1:
+                number += 1
+        except IndexError:
+            pass
+
+        self.number_of_mines = number
+
+    
+    def show(self):
+        if self.clicked == 1 and self.mine == 1:
+            screen.blit(self.surf_mine_explode, (self.position_x, self.position_y))
+        elif self.clicked == 0 and self.mine == 1 and defeat == 1:
+            screen.blit(self.surf_mine, (self.position_x, self.position_y))
+        elif self.clicked == 1 and self.mine == 0:
+            if self.number_of_mines == 0:
+                screen.blit(self.grid, (self.position_x, self.position_y))
+            elif self.number_of_mines == 1:
+                screen.blit(self.one, (self.position_x, self.position_y))
+            elif self.number_of_mines == 2:
+                screen.blit(self.two, (self.position_x, self.position_y))
+            elif self.number_of_mines == 3:
+                screen.blit(self.three, (self.position_x, self.position_y))
+            elif self.number_of_mines == 4:
+                screen.blit(self.four, (self.position_x, self.position_y))
+            elif self.number_of_mines == 5:
+                screen.blit(self.five, (self.position_x, self.position_y))
+            elif self.number_of_mines == 6:
+                screen.blit(self.six, (self.position_x, self.position_y))
+            elif self.number_of_mines == 7:
+                screen.blit(self.seven, (self.position_x, self.position_y))
+            elif self.number_of_mines == 8:
+                screen.blit(self.eight, (self.position_x, self.position_y))
+        else:
+            screen.blit(self.surf, (self.position_x, self.position_y))
+            #screen.blit(font.render(self.index_num, 1, pygame.Color("Red")), (self.position_x+11, self.position_y+11))
+
+    def click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.button_rect.collidepoint(x, y):
+                    self.clicked = 1
+                    if self.mine == 1:
+                        global defeat
+                        defeat = 1
 
 
-pygame.init()
 
-# Set up the drawing window
-screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-pygame.display.set_caption('Minesweeper')
+
 
 counter_x = 33
 counter_y = 149
@@ -51,6 +163,13 @@ for number in range(1,257):
     else:
         counter_x += 44
 
+for item in field_list:
+    item.index_number()
+
+
+for item in field_list:
+    item.scan()
+
 
 # Run until the user asks to quit
 running = True
@@ -71,11 +190,12 @@ while running:
     frame = pygame.image.load("frame.jpg").convert()
     screen.blit(frame, (0,0))
 
+    if defeat == 0:
+        for item in field_list:
+            item.click(event)
+
     for item in field_list:
-        if item.mine == 1:
-            screen.blit(item.surf_mine, (item.position_x,item.position_y))
-        else:
-            screen.blit(item.surf, (item.position_x,item.position_y))
+        item.show()
 
     pygame.display.flip()
 
